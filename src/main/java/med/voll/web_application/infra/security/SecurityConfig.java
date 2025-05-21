db5a88ca3d7c5f2fc02afac1c9e7b695834bb19c
Fioshi <guilhermefiochi@outlook.com>
2025-05-21 09:23:00 -0300

    feat: adicionado login personalizado de usuario exemplo mockado

--- src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
@@ -1,4 +1,21 @@
 package med.voll.web_application.infra.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user = User.builder().username("joao@mail.com").password("{noop}joao123").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
